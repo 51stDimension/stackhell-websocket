ff7943283bb903cf882a89b8f1085b21fe2aeffa
51stDimension <siddharthmohanty0812@gmail.com>
2025-05-03 00:07:31 +0530

    main: Final code for 2-way communication

--- src/main/java/org/dev/config/WebSocketConfig.java
+++ src/main/java/org/dev/config/WebSocketConfig.java
@@ -1,7 +1,6 @@
 package org.dev.config;
 
 
-import org.dev.handler.AssignPrincipalHandshakeHandler;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
